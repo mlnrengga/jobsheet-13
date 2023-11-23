8f52736817a63446991456c922b78e856c2fdd76
mlnrengga <mlnrengga@gmail.com>
2023-11-23 22:43:20 +0700

    Modify; UcapanTambahan

--- UcapanTerimaKasih.java
+++ UcapanTerimaKasih.java
@@ -12,7 +12,13 @@ public class UcapanTerimaKasih {
         System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+
         "You inspired me a love for learning and made me feel like i could ask you anything.");
     }
+    public static void UcapanTambahan(String tambahan){
+        System.out.println("Ucapan Tambahan: " + tambahan);
+    }
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        String tambahan = ("May you always be healthy.");
+        UcapanTambahan(tambahan);
+        
     }
 }
